7d2b330fdf1cdd8c8eb2269bc459eb153f081211
diasway <mdias2006s@gmail.com>
2025-05-01 23:50:53 +0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, Integer> scores = new HashMap<>();
-        
+        MyHashTable<MyTestingClass, String> table = new MyHashTable<>(100); // 100 бакетов
+        Random rand = new Random();
+
+        for (int i = 0; i < 10000; i++) {
+            int id = rand.nextInt(1000000);
+            table.put(new MyTestingClass(id), "student" + i);
+        }
+
+        System.out.println("elements: " + table.size());
+        table.printTable();
     }
 }
